2e5b631ecd02ecaa6287f43e3d0f36bf65511137
siyasiyasiya <siyacj08@gmail.com>
2022-08-28 22:21:10 -0500

    buttons change color when selected

--- app/src/main/java/com/example/mindlessapp/MainActivity.java
+++ app/src/main/java/com/example/mindlessapp/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.mindlessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private List<Button> buttons;
+    private static final int[] BUTTON_IDS = {
+            R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5,
+            R.id.button6, R.id.button7, R.id.button8, R.id.button9, R.id.button10,
+            R.id.button11, R.id.button12, R.id.button13, R.id.button14, R.id.button15,
+            R.id.button16, R.id.button17, R.id.button18, R.id.button19, R.id.button20,
+            R.id.button21, R.id.button22, R.id.button23, R.id.button24, R.id.button25,
+            R.id.button26, R.id.button27, R.id.button28, R.id.button29, R.id.button30,
+            R.id.button31, R.id.button32, R.id.button33, R.id.button34, R.id.button35
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //button arraylist
+        buttons = new ArrayList<>(BUTTON_IDS.length);
+        for(int id : BUTTON_IDS) {
+            Button button = findViewById(id);
+            buttons.add(button);
+        }
+
+        for(Button x: buttons){
+            x.setOnClickListener(view -> {
+                //button changes color when clicked
+                view.setSelected(!view.isSelected());
+            });
+        }
     }
 }
